2c862bacbc8f9e8cc753400dda49f6cf4f0d96c3
ssssnack <247770638@qq.com>
2018-02-24 16:27:14 +0800

    socket.io

--- demo5/wsServer.js
+++ demo5/wsServer.js
@@ -1,41 +1,13 @@
-var ws = require("nodejs-websocket")
-var PORT = 3000
-
-var clientCount = 0
-// Scream server example: "hi" -> "HI!!!"
-var server = ws.createServer(function (conn) {
-	console.log("New connection")
-	clientCount++
-	conn.nickname = 'user' + clientCount
-	var mes = {}
-	mes.type = 'enter'
-	mes.data = conn.nickname + 'comes in'
-	broadcast(JSON.stringify(mes))
-	conn.on("text", function (str) {
-		console.log("Received " + str)
-		var mes = {}
-		mes.type = 'message'
-		mes.data = conn.nickname+"says:"+str
-		broadcast(JSON.stringify(mes))
-	})
-	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
-		var mes = {}
-		mes.type = 'leave'
-		mes.data = conn.nickname + 'left'
-		broadcast(JSON.stringify(mes))
-	})
-	conn.on("error", function (err) {
-		console.log('handle err')
-		console.log(err)
-	})
-}).listen(PORT)
-
-console.log('websocket server listening on port' + PORT)
+var app = require('http').createServer();
+var io = require('socket.io')(app);
 
 
-function broadcast(str) {
-	server.connections.forEach(function (connection) {
-		connection.sendText(str)
-	})
-}
+
+app.listen(3000);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});	
